7efe56d03a235451baf7eeafaeb390cae0c00bfa
sswellington <sswellington@pm.me>
2023-04-17 09:02:15 -0300

    build: adding tailwind css to react
    installation and configuration of:
    - tailwind.css
    - cssnano
    
    reference:
     https://blog.logrocket.com/how-to-use-tailwind-css-react-vue-js/

---
+++ tailwind.config.js
@@ -0,0 +1,59 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    plugins: [],
+    theme: {
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',
+        },
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            blue: colors.blue,
+            emerald: colors.emerald,
+            gray: colors.gray,
+            indigo: colors.indigo,
+            yellow: colors.yellow,
+            white: colors.white,
+            error: {
+                DEFAULT: '#ba1a1a',
+                light: '#ba1a1a',
+                dark: '#ffb4ab',
+            },
+            on_error: {
+                DEFAULT: '#ffffff',
+                light: '#ffffff',
+                dark: '#690005',
+            },
+            error_container: {
+                DEFAULT: '#ffdad6',
+                light: '#ffdad6',
+                dark: '#93000a',
+            },
+            on_error_container: {
+                DEFAULT: '#410002',
+                light: '#410002',
+                dark: '#ffdad6',
+            },
+        },
+        fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+        extend: {
+            spacing: {
+                '128': '32rem',
+                '144': '36rem',
+            },
+            borderRadius: {
+                '4xl': '2rem',
+            }
+        }
+    }
+}
